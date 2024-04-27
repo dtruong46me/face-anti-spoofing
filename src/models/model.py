0420f7f7b334688c916f11dff6f85cb9eb2e4fbd
Dinh Truong Phan <dnhtruong46.cs.pyth@gmail.com>
2024-04-28 00:30:25 +0700

    model for classifier

--- src/models/model.py
+++ src/models/model.py
@@ -3,37 +3,37 @@ from typing import Literal
 from torch.optim import Adam
 import torch
 import torch.nn as nn
+from torchvision.models import resnext50_32x4d, ResNeXt50_32X4D_Weights
 from torchsummary import summary
 from pytorch_lightning.utilities.model_summary import summarize, ModelSummary
 
-class OriginResNeXT(nn.Module):
-    def __init__(self, input_shape):
-        super(OriginResNeXT, self).__init__()
-
-        self.resnext50 = torch.hub.load('pytorch/vision:v0.10.0', 'resnext50_32x4d', pretrained=True)
-
-        # Freeze trainable parameters
-        for name, param in self.resnext50.named_parameters():
-            print(">>>>>", name, type(name), param.size())
-            if name.startswith("fc"):
-                param.requires_grad = False
-                print("<<<<<<<<<<<<<<<")
-    
-    def forward(self, x):
-        return
-
 
 class SEResNeXT50(pl.LightningModule):
-    def __init__(self, input_shape, num_classes, **kwargs):
+    def __init__(self, input_shape, num_classes):
         super().__init__()
-        self.loss = nn.CrossEntropyLoss()
+        self.input_shape = input_shape
+        self.num_classes = num_classes
 
-        self.resnext50 = OriginResNeXT(input_shape)
-        self.fc = nn.Linear(2048, num_classes)
+        self.backbone = resnext50_32x4d(weights=ResNeXt50_32X4D_Weights.IMAGENET1K_V2)
+
+        for param in self.backbone.parameters():
+            param.requires_grad = False
+
+        self.dropout1 = nn.Dropout(p=0.5)
+
+        self.fc = nn.Linear(in_features=self.backbone.fc.in_features, 
+                            out_features=512)
+        
+        self.dropout2 = nn.Dropout(p=0.5)
+
+        self.classifier = nn.Linear(in_features=512,
+                                    out_features=num_classes)
+        
 
     def forward(self, x):
-        x = self.resnext50(x)
-        output = self.fc(x)
+        x = self.backbone(x)
+        output = self.dropout(x)
+        output = self.fc(output)
         return output
     
     def config_optimizers(self):
@@ -70,7 +70,7 @@ def load_model(modelname: Literal["seresnext50", "mobilenet", "feathernet"], inp
     
 
 if __name__=='__main__':
-    # origin = OriginResNeXT(input_shape=(224,224,3))
+
     model = load_model("seresnext50", input_shape=(3, 224, 224), num_classes=2)
 
     print(summarize(model))
