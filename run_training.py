6a56785f13684e2980fed58f7b54ebfcc9c991a3
Dinh Truong Phan <dnhtruong46.cs.pyth@gmail.com>
2024-05-15 16:53:50 +0700

    fix bug

--- run_training.py
+++ run_training.py
@@ -14,7 +14,7 @@ def run_training():
     parser.add_argument("--num_classes", type=int, default=2)
     
     parser.add_argument("--wandb_token", type=str, default="")
-    parser.add_argument("--wandb_name", type=str, default="model1")
+    parser.add_argument("--wandb_runname", type=str, default="model1")
 
     parser.add_argument("--modelname", type=str, default="seresnext50")
 
@@ -24,6 +24,10 @@ def run_training():
     parser.add_argument("--batch_size", type=int, default=64)
     parser.add_argument("--max_epochs", type=int, default=10)
     args = parser.parse_args()
+
+    print("=========================================")
+    print('\n'.join(f' + {k}={v}' for k, v in vars(args).items()))
+    print("=========================================")
 
     training_pipeline(args)
 
