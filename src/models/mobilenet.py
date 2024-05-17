7938dac304b0b578a2d8c4d461c5fb29779c42c0
VTMinh-20210597 <vutuanminh.work@gmail.com>
2024-05-17 16:10:59 +0700

    mobilenet_update

--- src/models/mobilenet.py
+++ src/models/mobilenet.py
@@ -7,6 +7,7 @@
 /_/  /_/\____/_.___/_/_/\___/_/ |_/\___/\__/     |___//____/
 
 """
+import torch
 import torch.nn as nn
 from torchsummary import summary
 from torchvision.models import mobilenet_v3_small
@@ -27,8 +28,25 @@ class MobileNetV2(nn.Module):
 
     def forward(self, x):
         x = self.mobilenet.features(x)
+        x = self.mobilenet.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.mobilenet.classifier(x)
         return x
+
 
 if __name__ == '__main__':
     model = MobileNetV2()
     summary(model, (3, 224, 224))
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    model.to(device)
+    # Step 2: Create a sample input tensor
+    input_tensor = torch.randn(1, 3, 224, 224).to(device)  # Batch size of 1, 3 color channels, 224x224 image size
+
+    # Step 3: Pass the input tensor through the model
+    output = model(input_tensor)
+
+    # Step 4: Print the output tensor
+    print("Model output:", output)
+    print("Output shape:", output.shape)
