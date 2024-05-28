0401f46ee73fcb764c094476cc14cd1e6f80ef18
Dinh Truong Phan <dnhtruong46.cs.pyth@gmail.com>
2024-05-28 23:30:49 +0700

    fix bug

--- src/models/ln_model.py
+++ src/models/ln_model.py
@@ -53,7 +53,7 @@ class ModelInterface(LightningModule):
     
     def configure_optimizers(self):
         return Adam(self.parameters(), lr=5e-5, weight_decay=1e-5)
-    
+
     def training_step(self, batch, batch_idx):
         loss, outputs, labels = self._common_step(batch, batch_idx)
 
