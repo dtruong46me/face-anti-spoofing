d4ef705af0b5c0ea0021e60d3f5392d56712935b
VTMinh-20210597 <vutuanminh.work@gmail.com>
2024-06-10 23:44:48 +0700

    Shufflenet

---
+++ src/models/ShuffleNet.py
@@ -0,0 +1,42 @@
+import torch
+import torch.nn as nn
+from torchvision.models import shufflenet_v2_x2_0
+from torchsummary import summary
+
+
+class ShuffleNet(nn.Module):
+    def __init__(self, input_shape=(3, 224, 224), num_classes=2):
+        super(ShuffleNet, self).__init__()
+        self.input_shape = input_shape
+        self.num_classes = num_classes
+        self.model = shufflenet_v2_x2_0(pretrained=True)
+
+        # Replace the classifier layer with a new one
+        num_features = self.model.fc.in_features  # shufflenet_v2_x2_0 uses 'fc' as the classifier layer
+        self.model.fc = nn.Linear(in_features=num_features, out_features=num_classes)
+
+    def forward(self, x):
+        x = self.model(x)
+        return x
+
+
+# if __name__ == '__main__':
+#     # Step 1: Instantiate the model
+#     model = ShuffleNet()
+#
+#     # Print model summary
+#     print(summary(model, (3, 224, 224)))
+#
+#     # Step 2: Set device
+#     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+#     model.to(device)
+#
+#     # Step 3: Create a sample input tensor
+#     input_tensor = torch.randn(1, 3, 224, 224).to(device)  # Batch size of 1, 3 color channels, 224x224 image size
+#
+#     # Step 4: Pass the input tensor through the model
+#     output = model(input_tensor)
+#
+#     # Step 5: Print the output tensor
+#     print("Model output:", output)
+#     print("Output shape:", output.shape)
--- src/pipelines/training_pipeline.py
+++ src/pipelines/training_pipeline.py
@@ -15,6 +15,7 @@ sys.path.insert(0, path)
 
 from models.ln_model import load_model, ModelInterface
 from models.resnext50 import SEResNeXT50
+from models.ShuffleNet import ShuffleNet
 
 from models.MobileLiteNet import *
 from models.feathernet import *
@@ -64,6 +65,8 @@ def training_pipeline(args: argparse.Namespace):
     # Load FeatherNet
     if args.modelname == "feathernet":
         backbone = FeatherNetB()
+    if args.modelname == "shufflenet":
+        backbone = ShuffleNet()
 
     # Load backbone
     backbone = load_backbone(args)
