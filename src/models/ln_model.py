cdc0c0fe46ce795b15439468ad2b720c0e008742
WValleyy <101030063+WValleyy@users.noreply.github.com>
2024-06-22 12:11:23 +0700

    Update ln_model.py

--- src/models/ln_model.py
+++ src/models/ln_model.py
@@ -92,7 +92,7 @@ class ModelInterface(LightningModule):
         images, labels = batch
         labels = labels.squeeze(0).float()
 
-        weights = [0.9, 0.1]
+        weights = [0.85, 0.15]
         weights = torch.FloatTensor(weights).cuda()
 
         outputs = self.forward(images)
